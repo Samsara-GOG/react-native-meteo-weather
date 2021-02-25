7ff1f1f1b14177cd95259aade5a44c7fd244e617
Samsara <samsaraonaya@gmail.com>
2021-02-25 19:38:44 +0100

    Nettoyage de code

--- components/OtherDay.js
+++ components/OtherDay.js
@@ -1,10 +1,10 @@
-import React from "react";
-import { StyleSheet, Image, Text, View } from "react-native";
+import React from 'react';
+import { StyleSheet, Image, Text, View } from 'react-native';
 
 import {
   useResponsiveHeight,
   useResponsiveWidth,
-} from "react-native-responsive-dimensions";
+} from 'react-native-responsive-dimensions';
 
 export default function OtherDay(props) {
   let height = useResponsiveHeight(25);
@@ -30,43 +30,43 @@ export default function OtherDay(props) {
 const styles = StyleSheet.create({
   otherDay: {
     flex: 1,
-    backgroundColor: "#EFFBFE",
-    color: "black",
-    flexDirection: "row",
-    justifyContent: "center",
+    backgroundColor: 'rgb(162, 195, 250)',
+    color: 'black',
+    flexDirection: 'row',
+    justifyContent: 'center',
     height: 100,
     borderBottomWidth: 1,
-    borderBottomColor: "#C3D2FF",
+    borderBottomColor: '#C3D2FF',
   },
   condition: {
     paddingTop: 1,
-    textTransform: "capitalize",
+    textTransform: 'capitalize',
   },
   date: {
-    textTransform: "capitalize",
+    textTransform: 'capitalize',
     fontSize: 18,
   },
   left: {
     flex: 1.6,
-    flexDirection: "row",
+    flexDirection: 'row',
   },
   middle: {
-    textTransform: "capitalize",
+    textTransform: 'capitalize',
     flex: 3.8,
-    alignSelf: "center",
-    flexDirection: "column",
+    alignSelf: 'center',
+    flexDirection: 'column',
     marginLeft: 15,
   },
   right: {
     flex: 0.8,
-    alignSelf: "center",
+    alignSelf: 'center',
     paddingLeft: 5,
     paddingRight: 10,
   },
   stretch: {
     width: 100,
     height: 100,
-    resizeMode: "stretch",
+    resizeMode: 'stretch',
   },
   temperature: {
     fontSize: 18,
